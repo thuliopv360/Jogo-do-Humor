7f8ab67cef122fc2101d9e31bc833368c4bba194
Thulio Paiva <thuliopv360@gmail.com>
2022-04-13 20:49:29 -0300

    alteraçao js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
+let img = document.getElementById("teste");
+
+document.getElementById("textonome").innerHTML = "Itadori";
+document.getElementById("textosituacao").innerHTML = "Humano";
+
+let momento = false;
+
 function mudarNome(id) {
-    let indice = 0;
-    while (indice < 3) {
-        indice++;
-        if (indice == 1) {
-            document.getElementById("textonome").innerHTML = "Itadori";
-            document.getElementById("textosituacao").innerHTML = "Humano";
-            indice++;
-        } else if (indice == 2) {
-            document.getElementById("textonome").innerHTML = "Itadori lutando";
-            document.getElementById("textosituacao").innerHTML = "Humano";
-        }
+    while (momento == false) {
+        img.src = "https://i1.sndcdn.com/artworks-T5YyETaYHzOdTMKp-Mfzvhg-t500x500.jpg"
+        document.getElementById("textonome").innerHTML = "Itadori";
+        document.getElementById("textosituacao").innerHTML = "Lutando";
+        momento = true;
+    }
+    if (momento == true) {
+        img.src = "https://p16.resso.me/img/tos-alisg-v-2102/1a33c2771b5f470d9e95d0d8cfcf21b6~c5_500x500.jpg"
+        document.getElementById("textonome").innerHTML = "Itadori";
+        document.getElementById("textosituacao").innerHTML = "Lutando";
+        momento = false;
     }
 }
 // document.getElementById("textonome").innerHTML = "Itadori";
