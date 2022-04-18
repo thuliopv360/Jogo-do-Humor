db241c0b9caa810e9eb3ac739d658c85f700711e
Thulio Paiva <thuliopv360@gmail.com>
2022-04-18 19:13:16 -0300

    resposividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,8 @@
-// let img = document.getElementById("teste");
-
-// document.getElementById("textonome").innerHTML = "Itadori";
-// document.getElementById("textosituacao").innerHTML = "Humano";
-
-// let momento = false;
-
-// function mudarNome(id) {
-//     while (momento == false) {
-//         img.src = "https://i1.sndcdn.com/artworks-T5YyETaYHzOdTMKp-Mfzvhg-t500x500.jpg"
-//         document.getElementById("textonome").innerHTML = "Itadori";
-//         document.getElementById("textosituacao").innerHTML = "Lutando";
-//         momento = true;
-//     }
-//     if (momento == true) {
-//         img.src = "https://p16.resso.me/img/tos-alisg-v-2102/1a33c2771b5f470d9e95d0d8cfcf21b6~c5_500x500.jpg"
-//         document.getElementById("textonome").innerHTML = "Itadori";
-//         document.getElementById("textosituacao").innerHTML = "Lutando";
-//         momento = false;
-//     }
-// }
 document.getElementById("textonome").innerHTML = "Itadori";
 document.getElementById("textosituacao").innerHTML = "Humano";
 
 const span = document.querySelector("span");
 const img = document.querySelector("#imagemFinal");
-
-// div_id.addEventListener("click", () => {
-//     console.log("Eu fui clicado");
-// })
 
 
 const imgList = ["./assets/img/itadori.png", "./assets/img/itadoriLutando.png", "./assets/img/sukuna.png", "./assets/img/sukunaLutando.png"];
